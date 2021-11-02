80bb6b3b63e08f7c02bf5a4e33357b9ff5576f1e
Oleksii Markhovskyi <oleksii.markhovskyi@here.com>
2021-11-02 16:42:34 +0100

    store a modified template into user's workspace

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,52 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand(
-    'weekly-journal.weeklyJournal',
-    () => {
-      vscode.window.showInformationMessage('sample message from Weekly Journal');
-    },
-  );
+import * as path from 'path';
 
-	context.subscriptions.push(disposable);
+const templateFolder = 'templates'; // inside extension codebase
+const targetFolder = 'journals'; // inside user's workspace
+
+async function createJournalFromTemplate(templatePath: string, weekIndex: number): Promise<void> {
+  if (!vscode.workspace.workspaceFolders) {
+    vscode.window.showWarningMessage(
+      `Please open some folder, so weekly logs could be stored under "${targetFolder}/" inside.`,
+    );
+    return;
+  }
+
+  const weekIndexFormatted = weekIndex.toString().padStart(2, '0');
+
+  const templateUri = vscode.Uri.file(templatePath);
+  const targetUri = vscode.Uri.file(path.join(
+    vscode.workspace.workspaceFolders[0].uri.path,
+    targetFolder,
+    `week-${weekIndexFormatted}.md`,
+  ));
+
+  // await vscode.workspace.fs.copy(templateUri, targetUri, { overwrite: true });
+
+  const templateContent = Buffer
+    .from(await vscode.workspace.fs.readFile(templateUri))
+    .toString('utf8');
+
+  const targetContent = templateContent
+    .replace('{NN}', weekIndexFormatted);
+
+  await vscode.workspace.fs.writeFile(targetUri, Buffer.from(targetContent, 'utf8'));
+  await vscode.window.showTextDocument(targetUri);
 }
 
-export function deactivate() {
+export function activate(context: vscode.ExtensionContext): void {
+  const disposable = vscode.commands.registerCommand(
+    'weekly-journal.weeklyJournal',
+    () => createJournalFromTemplate(
+      context.asAbsolutePath(path.join(templateFolder, 'week-NN.md')),
+      1,
+    ),
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate(): void {
   console.log('deactivating');
 }
---
+++ templates/week-NN.md
@@ -0,0 +1,32 @@
+# week {NN}: {YYYY-MM-DD} - {YYYY-MM-DD}
+
+todo:
+* TBD
+
+## Monday, {YYYY-MM-DD}
+
+- [ ] planning <- {HH:MM}
+- [ ] rotate journals
+
+## Tuesday, {YYYY-MM-DD}
+
+- [ ] `{task-id}` - {task-description}
+  - [ ] debug
+  - [ ] implement
+  - [ ] test
+
+## Wednesday, {YYYY-MM-DD}
+
+- [ ] `{task-id}` - {task-description}
+  - [ ] review
+  - [ ] discuss
+  - [ ] merge
+
+## Thursday , {YYYY-MM-DD}
+
+- [ ] demo <- {HH:MM}
+- [ ] retro <- {HH:MM}
+
+## Friday, {YYYY-MM-DD}
+
+- [ ] design review <- {HH:MM}
